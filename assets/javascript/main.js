f1784eb34e98ac48df122f763e45601c0b538e35
Antonia Cerda <antoniacerdag@gmail.com>
2017-04-27 13:36:59 -0300

    poniendo bonito el archivo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -105,4 +105,12 @@ function printHTML (html){
 }
 
 
+function NuevoUser(userId,id,title,completed){
+    this.userId = userId;
+    this.id = id;
+    this.title = title;
+    this.completed = completed;
+};
+
+
 
